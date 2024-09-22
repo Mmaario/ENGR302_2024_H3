a2a153384d59851af935460b6b58f221487eb9d0
Louis Smith <louis.a.t.smith98@gmail.com>
2024-09-23 08:21:37 +1200

    updated command setup and welcome message

--- Code/message.c
+++ Code/message.c
@@ -24,6 +24,7 @@ void send_file_contents(int connfd, const char *filename) {
     fclose(file);
 }
 
+// Function to handle client commands
 void handle_client(int connfd) {
     char buff[MAX];
 
@@ -38,20 +39,25 @@ void handle_client(int connfd) {
         read(connfd, buff, sizeof(buff));
         printf("From client: %s\n", buff);
 
-        // Command to query current mode
-        if (strncmp("get_mode", buff, 8) == 0) {
+        // Convert message to lowercase for case insensitivity
+        for (int i = 0; buff[i]; i++) {
+            buff[i] = tolower(buff[i]);
+        }
+
+        // Command to check current mode (e.g., "status")
+        if (strncmp("status", buff, 6) == 0) {
             int current_mode = get_current_mode();
-            sprintf(buff, "Current mode is %d\n", current_mode);
+            sprintf(buff, "Current gain mode is %d\n", current_mode);
             write(connfd, buff, sizeof(buff));
         }
-        // Command to switch mode (e.g., "set_mode 3")
-        else if (strncmp("set_mode", buff, 8) == 0) {
-            int mode = buff[9] - '0';  // Get the mode number from the message
-            if (mode >= 0 && mode < 6) {
+        // Command to change mode (e.g., "gain 2")
+        else if (strncmp("gain", buff, 4) == 0) {
+            int mode = buff[5] - '0';  // Get the mode number from the message
+            if (mode >= 1 && mode <= 4) {
                 set_mode(mode);
-                sprintf(buff, "Switched to mode %d\n", mode);
+                sprintf(buff, "Switched to gain mode %d\n", mode);
             } else {
-                sprintf(buff, "Invalid mode. Choose between 0 and 5.\n");
+                sprintf(buff, "Invalid gain mode. Choose between 1 and 4.\n");
             }
             write(connfd, buff, sizeof(buff));
         }
